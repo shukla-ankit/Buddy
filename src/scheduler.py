fe808442a3b2933dee7c49fe12d0d8f4a58e3387
Ankit Shukla <shukla.ankit@protonmail.com>
2021-09-02 06:02:46 -0400

    .

--- src/scheduler.py
+++ src/scheduler.py
@@ -26,8 +26,21 @@ class Scheduler:
         permute(0)
         self.__all_unique_permutations = list(set(map(lambda x: tuple(x), self.__all_unique_permutations)))
 
+    def get_pairs(self, list_members_names: List[str]) -> List[List[str]]:
+        list_pairs: List[List[str]] = []
+        is_size_uneven = len(list_members_names) / 2 != 0
+        pair = []
+        for i in range(len(list_members_names)):
+            if len(pair) == 2:
+                list_pairs.append(pair)
+                pair = []
+            pair.append(list_members_names[i])
+        if is_size_uneven:
+            list_pairs[- 1].append(list_members_names[-1])
+        return list_pairs
+
     def generate_schedule(self) -> List[List[str]]:
-        schedule: List[List[str]] = []
+        schedule: List[List[List[str]]] = []
         seed(7) # 7 is prime, so chosen for seed generation
         list_existing_indices = []
         num_permutations = len(self.__all_unique_permutations)
@@ -35,10 +48,10 @@ class Scheduler:
             if len(list_existing_indices) == i:
                 list_existing_indices.clear()
             # r = randint(num_permutations)
-            r = rand.roll_dice_MaxMin(minimum_number=0,maximum_number=num_permutations)
+            r = rand.roll_dice_MaxMin(minimum_number=0, maximum_number=num_permutations)
             while r in list_existing_indices:
                 # r = randint(num_permutations)
                 r = rand.roll_dice_MaxMin(minimum_number=0, maximum_number=num_permutations)
             list_existing_indices.append(r)
-            schedule.append(self.__all_unique_permutations[r])
+            schedule.append(self.get_pairs(self.__all_unique_permutations[r]))
         return schedule
