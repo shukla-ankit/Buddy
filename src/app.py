241e3aea2b89e4b391b0d2abfea6db61b3f50e3d
Ankit Shukla <shukla.ankit@protonmail.com>
2021-09-02 08:22:13 -0400

    code clean up

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import time
 import sys
 from team import Team
 from scheduler import Scheduler
@@ -8,6 +9,7 @@ def main():
     if len(sys.argv) != 2:
         print('Usage: python3 app.py <json-file-path>')
         sys.exit(1)
+    start_time = time.time()
     json_file_path = sys.argv[1]
     team = Team('ASR')
     with open(json_file_path) as json_file:
@@ -15,13 +17,19 @@ def main():
     team.add_members_by_json(members_json)
     team.print_team()
     print("\n\n")
-    scheduler = Scheduler(team.get_subscribed_team_members_names(), 5)
+    list_members = team.get_subscribed_team_members_names()
+    scheduler = Scheduler(len(list_members), 5)
     schedule = scheduler.generate_schedule()
     i = 0
     for pairs in schedule:
         i = i + 1
-        print('Day ' + str(i) + ': ', end='')
-        print(pairs)
+        print('Day ' + str(i) + ': ')
+        for pair in pairs:
+            if len(pair) == 2:
+                print('    ['+ list_members[pair[0]]+ ',' +  list_members[pair[1]] + ']')
+            else:
+                print('    ['+ list_members[pair[0]]+ ',' +  list_members[pair[1]] + list_members[pair[2]] + ']')
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == '__main__':
--- src/scheduler.py
+++ src/scheduler.py
@@ -3,49 +3,46 @@ from random import seed, randint
 import random_dice_roller as rand
 
 class Scheduler:
-    def __init__(self, list_team_members_names: List[str], n_days: int):
-        if len(list_team_members_names) < 3:
-            print('Please provide list team members count more than 3!')
+    def __init__(self, size_team: int, n_days: int):
+        if size_team < 3:
+            print('Please provide team with more members than 3!')
             exit(0)
         self.__n_days = n_days
         self.__all_unique_permutations = []
-        self.permuteUnique(list_team_members_names)
+        self.permuteUnique([i for i in range(size_team)])
 
-    def permuteUnique(self, list_team_members_names: List[str]):
+    def permuteUnique(self, list_members_id: List[int]):
         def permute(i: int):
-            if i == len(list_team_members_names):
-                self.__all_unique_permutations.append(list_team_members_names[:])
+            if i == len(list_members_id):
+                self.__all_unique_permutations.append(list_members_id[:])
 
-            for j in range(i, len(list_team_members_names)):
-                list_team_members_names[i], list_team_members_names[j] = list_team_members_names[j], \
-                                                                         list_team_members_names[i]
+            for j in range(i, len(list_members_id)):
+                list_members_id[i], list_members_id[j] = list_members_id[j], list_members_id[i]
                 permute(i + 1)
-                list_team_members_names[i], list_team_members_names[j] = list_team_members_names[j], \
-                                                                         list_team_members_names[i]
+                list_members_id[i], list_members_id[j] = list_members_id[j], list_members_id[i]
 
         permute(0)
         self.__all_unique_permutations = list(set(map(lambda x: tuple(x), self.__all_unique_permutations)))
 
-    def get_pairs(self, list_members_names: List[str]) -> List[List[str]]:
-        list_pairs: List[List[str]] = []
-        is_size_uneven = len(list_members_names) / 2 != 0
+    def get_pairs(self, list_members_id: List[int]) -> List[List[int]]:
+        list_pairs_id: List[List[int]] = []
+        is_size_uneven = len(list_members_id) / 2 != 0
         pair = []
-        for i in range(len(list_members_names)):
+        for i in range(len(list_members_id)):
             if len(pair) == 2:
                 pair.sort()
-                list_pairs.append(pair)
+                list_pairs_id.append(pair)
                 pair = []
-            pair.append(list_members_names[i])
+            pair.append(list_members_id[i])
         if is_size_uneven:
-            k = rand.roll_dice_MaxMin(minimum_number=0, maximum_number=len(list_pairs)-1)
-            print (k)
-            list_pairs[k].append(list_members_names[-1])
-            list_pairs[k].sort()
-        list_pairs.sort()
-        return list_pairs
+            k = rand.roll_dice_MaxMin(minimum_number=0, maximum_number=len(list_pairs_id)-1)
+            list_pairs_id[k].append(list_members_id[-1])
+            list_pairs_id[k].sort()
+        list_pairs_id.sort()
+        return list_pairs_id
 
     def generate_schedule(self) -> List[List[str]]:
-        schedule: List[List[List[str]]] = []
+        schedule: List[List[List[int]]] = []
         seed(7) # 7 is prime, so chosen for seed generation
         list_existing_indices = []
         num_permutations = len(self.__all_unique_permutations)
