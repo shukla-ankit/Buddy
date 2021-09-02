8581c13902634f03eb7a7b53a99b80bc57480afc
Ankit Shukla <shukla.ankit@protonmail.com>
2021-09-02 06:42:30 -0400

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # BuddySystem
+A simple app to randomly create pairs of 2 or maximum 3 members from a team
+
+## Motivation
+As most of the people are Working-From-Home these days, social interactions have become very limited. This app can create pairs of team members random, like in real life, to connect for couple of minutes of conversation, maybe over coffee.
+
+## How to use
+Step 1. Update ./data/members.json file with team members information
+Step 2.
+```
+python3 ./src/app.py
+```
+
+## Requisite
+```
+pip install random_dice_roller
+```
--- src/app.py
+++ src/app.py
@@ -1,17 +1,27 @@
+import sys
 from team import Team
 from scheduler import Scheduler
 import json
 
 
 def main():
+    if len(sys.argv) != 2:
+        print('Usage: python3 app.py <json-file-path>')
+        sys.exit(1)
+    json_file_path = sys.argv[1]
     team = Team('ASR')
-    with open('../data/members.json') as json_file:
+    with open(json_file_path) as json_file:
         members_json = json.load(json_file)
     team.add_members_by_json(members_json)
     team.print_team()
     print("\n\n")
     scheduler = Scheduler(team.get_subscribed_team_members_names(), 5)
-    print(scheduler.generate_schedule())
+    schedule = scheduler.generate_schedule()
+    i = 0
+    for pairs in schedule:
+        i = i + 1
+        print('Day ' + str(i) + ': ', end='')
+        print(pairs)
 
 
 if __name__ == '__main__':
