1a29b66fbd9f06983a13263cc2b28d94455f4272
Ankit Shukla <shukla.ankit@protonmail.com>
2021-09-02 02:40:52 -0400

    readability

--- src/team.py
+++ src/team.py
@@ -6,14 +6,14 @@ from member import Member
 
 class Team:
     def __init__(self, team_name: str):
-        self.__name = team_name
+        self.__t_name = team_name
         self.__d_members: Dict[str, Member] = {}
 
     def get_name(self):
-        return self.__name
+        return self.__t_name
 
     def set_name(self, team_name: str):
-        self.__name = team_name
+        self.__t_name = team_name
 
     def add_member(self, f_name: str, l_name: str, email: str):
         try:
@@ -42,40 +42,32 @@ class Team:
         if idx is not None:
             del self.__d_members[email]
 
-    def unsubscribe_by_email(self, email: str):
+    def __get_member(self, key: str):
         try:
-            idx = self.__d_members.get(email)
-            if idx is not None:
-                self.__d_members[email].unsubscribe()
-            else:
-                raise Exception('Entered email {' + email + '} not found in existing employee pool!')
+            if self.__d_members.get(key) is None:
+                raise Exception('Entered email {' + key + '} not found in employee pool!')
                 exit(0)
+            return self.__d_members[key]
         except Exception as error:
-            print('unsubscribe_by_email : ' + repr(error))
+            print('__get_member : ' + repr(error))
+
+    def unsubscribe_by_email(self, email: str):
+        self.self.__get_member(email).unsubscribe()
 
     def resubscribe_by_email(self, email: str):
-        try:
-            idx = self.__d_members.get(email)
-            if idx is not None:
-                self.__d_members[email].subscribe()
-            else:
-                raise Exception('Entered email {' + email + '} not found in existing employee pool!')
-                exit(0)
-        except Exception as error:
-            print('resubscribe_by_email : ' + repr(error))
+        self.self.__get_member(email).subscribe()
 
     def print_team(self):
-        print('\nTeam ', self.__name, '\n--------------------------------------')
-        for key in self.__d_members:
-            if self.__d_members[key].has_subscribed():
+        print('\nTeam ', self.__t_name, '\n--------------------------------------')
+        for email in self.__d_members:
+            subscription_status = ' x '
+            if self.__get_member(email).has_subscribed():
                 subscription_status = ' - '
-            else:
-                subscription_status = ' x '
-            print(subscription_status + self.__d_members[key].get_full_name())
+            print(subscription_status + self.__get_member(email).get_full_name())
 
     def get_subscribed_team_members_names(self):
         list_team_members_names: str = []
-        for key in self.__d_members:
-            if self.__d_members[key].has_subscribed():
-                list_team_members_names.append(self.__d_members[key].get_full_name())
+        for email in self.__d_members:
+            if self.__get_member(email).has_subscribed():
+                list_team_members_names.append(self.__get_member(email).get_full_name())
         return list_team_members_names
