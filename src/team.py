5805fd7ba8d73a0925a3a9a3c37af30dcf0e0752
Ankit Shukla <shukla.ankit@protonmail.com>
2021-09-02 02:42:52 -0400

    readability

--- src/team.py
+++ src/team.py
@@ -7,7 +7,7 @@ from member import Member
 class Team:
     def __init__(self, team_name: str):
         self.__t_name = team_name
-        self.__d_members: Dict[str, Member] = {}
+        self.__map_emails_to_members: Dict[str, Member] = {}
 
     def get_name(self):
         return self.__t_name
@@ -17,8 +17,8 @@ class Team:
 
     def add_member(self, f_name: str, l_name: str, email: str):
         try:
-            if self.__d_members.get(email) is None:
-                self.__d_members[email] = Member(f_name, l_name, email)
+            if self.__map_emails_to_members.get(email) is None:
+                self.__map_emails_to_members[email] = Member(f_name, l_name, email)
             else:
                 raise Exception('Entered email {' + email + '} already exists in employee pool!')
                 exit(0)
@@ -29,7 +29,7 @@ class Team:
         try:
             for elem in members_json['Members']:
                 if elem is not None:
-                    if self.__d_members.get(elem['email']) is None:
+                    if self.__map_emails_to_members.get(elem['email']) is None:
                         self.add_member(elem['f_name'], elem['l_name'], elem['email'])
                     else:
                         raise Exception('Entered email {' + elem['email'] + '} already exists in employee pool!')
@@ -38,16 +38,16 @@ class Team:
             print('add_members_by_json : ' + repr(error))
 
     def remove_by_email(self, email: str):
-        idx = self.__d_members.get(email)
+        idx = self.__map_emails_to_members.get(email)
         if idx is not None:
-            del self.__d_members[email]
+            del self.__map_emails_to_members[email]
 
     def __get_member(self, key: str):
         try:
-            if self.__d_members.get(key) is None:
+            if self.__map_emails_to_members.get(key) is None:
                 raise Exception('Entered email {' + key + '} not found in employee pool!')
                 exit(0)
-            return self.__d_members[key]
+            return self.__map_emails_to_members[key]
         except Exception as error:
             print('__get_member : ' + repr(error))
 
@@ -59,7 +59,7 @@ class Team:
 
     def print_team(self):
         print('\nTeam ', self.__t_name, '\n--------------------------------------')
-        for email in self.__d_members:
+        for email in self.__map_emails_to_members:
             subscription_status = ' x '
             if self.__get_member(email).has_subscribed():
                 subscription_status = ' - '
@@ -67,7 +67,7 @@ class Team:
 
     def get_subscribed_team_members_names(self):
         list_team_members_names: str = []
-        for email in self.__d_members:
+        for email in self.__map_emails_to_members:
             if self.__get_member(email).has_subscribed():
                 list_team_members_names.append(self.__get_member(email).get_full_name())
         return list_team_members_names
